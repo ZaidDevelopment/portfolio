0ad16375d7542fcfed1f34517d553a92b9d71df5
Zaid <zaid.a@hotmail.com>
2024-09-16 22:23:43 +0100

    Set the Projects component grid to sm:grid-cols-2.

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -13,7 +13,7 @@ const Projects = () => {
           </span>
         </h1>
         {/* Project Cards */}
-        <div className="mt-12 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <div className="mt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {PROJECT_CARDLIST.map((project) => (
             <ProjectCard
               key={project.title} // Using title as key as titles are unique
